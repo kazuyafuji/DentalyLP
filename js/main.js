7a13f5d010fc6277084c0d16072f1363e72a12ec
kazuya fujiwara <kazuyafujiwara@kazuyanoMacBook-Pro.local>
2022-09-27 01:06:00 +0900

    header jQuery added

--- js/main.js
+++ js/main.js
@@ -20,14 +20,29 @@ if (window.scrollY > 66.5) {
 }
 */
 
-/*次、headerのクラスの付け替えから　これは機能してない*/
+/*次、headerのクラスの付け替えから　
+これは機能してない*/
 $(function () {
+  /*
+    headerという変数をjQueryの型で指定していないから機能しない
   var header = document.querySelector("header");
+  さらに、scrollTop()として関数を実行しないと数値は取得できない！
+  */
+  var header = $("header");
+  /*
+  確認用script
   $(window).scroll(function () {
-    if ($(this).scrollTop > 67) {
+    console.log($(this).scrollTop());
+    console.log(header.hasClass("header-normal"));
+    console.log("white", header.hasClass("header-white"));
+  });
+  */
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
       header.addClass("header-white");
     } else {
-      header.removeClass();
+      header.removeClass("header-white");
     }
   });
 });
